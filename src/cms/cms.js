80f01309e22047757f49c83c9715bd5da9bf3143
Rick <richard.j.nielsen@gmail.com>
2019-08-04 09:17:24 -0300

    Merge branch 'master' of https://github.com/trojans10/gatsby-starter-netlify-cms
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -7,8 +7,6 @@ import BlogPostPreview from './preview-templates/BlogPostPreview'
 import LoanPostPreview from './preview-templates/LoanPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import IndexPagePreview from './preview-templates/IndexPagePreview'
-import TestPagePreview from './preview-templates/TestPagePreview'
-
 
 CMS.registerMediaLibrary(uploadcare);
 CMS.registerMediaLibrary(cloudinary);
@@ -18,31 +16,3 @@ CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('pages', BlogPostPreview)
 CMS.registerPreviewTemplate('pages', LoanPostPreview)
-CMS.registerPreviewTemplate('test', TestPreview)
-CMS.registerEditorComponent({
-  // Internal id of the component
-  id: "youtube",
-  // Visible label
-  label: "Youtube",
-  // Fields the user need to fill out when adding an instance of the component
-  fields: [{name: 'id', label: 'Youtube Video ID', widget: 'string'}],
-  // Pattern to identify a block as being an instance of this component
-  pattern: /^youtube (\S+)$/,
-  // Function to extract data elements from the regexp match
-  fromBlock: function(match) {
-    return {
-      id: match[1]
-    };
-  },
-  // Function to create a text block from an instance of this component
-  toBlock: function(obj) {
-    return 'youtube ' + obj.id;
-  },
-  // Preview output for this component. Can either be a string or a React component
-  // (component gives better render performance)
-  toPreview: function(obj) {
-    return (
-      '<img src="http://img.youtube.com/vi/' + obj.id + '/maxresdefault.jpg" alt="Youtube Video"/>'
-    );
-  }
-});
